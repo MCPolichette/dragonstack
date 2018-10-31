a313c75ac338ea66dc56ae260c764061e6be230b
MCPolichette <mark@polichette.com>
2018-10-31 12:11:12 -0600

    new section.  small updates to configs/generation js files

---
+++ backend/config.js
@@ -0,0 +1,14 @@
+const SECONDS = 1000;
+const MINS = SECONDS * 60;
+const HOURS = MINS * 60;
+const DAYS = HOURS * 24;
+
+const REFRESH_RATE = 5 // units
+
+module.exports = {
+    SECONDS,
+    MINS,
+    HOURS,
+    DAYS,
+    REFRESH_RATE
+};
