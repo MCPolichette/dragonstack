f68ec90f0bbe188c082cc61ab065faa5e25a1324
MCPolichette <mark@polichette.com>
2018-11-04 17:38:11 -0700

    express started

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,12 @@
-const Dragon = require("./dragon.js");
-const Generation = require("./generation");
+const express = require('express');
 const GenerationEngine = require('./engine.js')
 
-const engine = new GenerationEngine()
-console.log("hello world");
-
+const app = express();
+const engine = new GenerationEngine();
+const port = 3000;
 engine.start();
-setTimeout(() => { engine.stop(); }, 20000);
-// console.log('generation', generation);
-// const doc = generation.newDragon();
-// console.log("doc", doc)
 
-// const chester = new Dragon({
-//     birthdate: new Date(),
-//     nickname: "Cheese"
-// });
-// const bill = new Dragon({
-//     nickname: "Bill",
-//     birthdate: new Date(),
-//     traits: [{ traitType: 'backgroundColor', traitValue: 'green' }]
-// });
-// const mimar = new Dragon();
-
-// setTimeout(() => {
-//     const gooby = generation.newDragon();
-//     console.log("gooby", gooby);
-// }, 15000);
-
-
-// // console.log(bill)
-// console.log("CheeseBurger", chester);
-// console.log("Bill", bill);
-// console.log("CheeseBurger", chester);
-// console.log("Mimar", mimar);
+app.get('/dragon/new', (req, res) => {
+    res.json({ dragon: engine.generation.newDragon() });
+});
+app.listen(port, () => console.log(`listening on port ${port}`))
