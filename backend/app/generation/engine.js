8c25c86f459a6321e13eb9d7b121ca105aa978e5
MCPolichette <mark@polichette.com>
2018-11-14 16:40:21 -0700

    more backend updates

--- backend/app/generation/engine.js
+++ backend/app/generation/engine.js
@@ -14,16 +14,23 @@ class GenerationEngine {
         clearTimeout(this.timer);
     }
     buildNewGeneration() {
-        this.generation = new Generation();
+        const generation = new Generation();
 
-        GenerationTable.storeGeneration(this.generation);
+        GenerationTable.storeGeneration(this.generation)
+            .then(({ generationId }) => {
+                this.generation = generation;
 
-        console.log('new generation', this.generation)
+                this.generation.generationId = generationId
+                console.log('new generation', this.generation)
 
-        this.timer = setTimeout(
-            () => this.buildNewGeneration(),
-            this.generation.expiration.getTime() - Date.now()
-        );
+                this.timer = setTimeout(
+                    () => this.buildNewGeneration(),
+                    this.generation.expiration.getTime() - Date.now()
+                );
+
+            }).catch(error => console.error(error));
+
+
 
     }
 }
